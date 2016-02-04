295adda01f8dbcc2ca272b7871f72f24ea6b33a5
Paddock <william.paddock1@bz-cs-cslab2.coe.montana.edu>
2016-02-04 14:54:29 -0700

    Working to add multithreading will add cuda threading

--- hashCracker/src/cracker.java
+++ hashCracker/src/cracker.java
@@ -5,6 +5,35 @@ import java.util.Hashtable;
 
 public class cracker
 {
+    private Thread t;
+    private String threadName;
+    RunnableHash (String digest){
+        threadName = hash;
+        System.out.print("Creating "+threadName);
+    }
+    public void run() {
+        System.out.println("Running " +  threadName );
+        try {
+            for(int i = 4; i > 0; i--) {
+                System.out.println("Thread: " + threadName + ", " + i);
+                // Let the thread sleep for a while.
+                Thread.sleep(50);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " +  threadName + " interrupted.");
+        }
+        System.out.println("Thread " +  threadName + " exiting.");
+    }
+
+    public void start ()
+    {
+        System.out.println("Starting " +  threadName );
+        if (t == null)
+        {
+            t = new Thread (this, threadName);
+            t.start ();
+        }
+    }
     public static void main(String[] args)throws Exception
     {
         MessageDigest md = MessageDigest.getInstance("MD5");
