a45ef5f8247eea894e908e3198a01bc0129ea916
retsu8 <teck.geek@hotmail.com>
2016-02-04 00:09:36 -0700

    Basic Ideas
    The basic idea for the dictionary checker.

--- hashCracker/src/cracker.java
+++ hashCracker/src/cracker.java
@@ -1,36 +1,29 @@
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.security.MessageDigest;
+import java.util.Hashtable;
 
 public class cracker
 {
     public static void main(String[] args)throws Exception
     {
         MessageDigest md = MessageDigest.getInstance("MD5");
-        FileInputStream fis = new FileInputStream("c:\\loging.log");
+        BufferedReader dictionary = new BufferedReader(new FileReader(args[0]));
+        BufferedReader md5list = new BufferedReader(new FileReader(args[1]));
 
-        byte[] dataBytes = new byte[1024];
-
-        int nread = 0;
-        while ((nread = fis.read(dataBytes)) != -1) {
-            md.update(dataBytes, 0, nread);
-        };
-        byte[] mdbytes = md.digest();
-
-        //convert the byte to hex format method 1
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < mdbytes.length; i++) {
-            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+        int i = 0;
+        Hashtable<String, String> crackedHash[] = new Hashtable[md5list.toString().length()];
+        while (md5list.readLine() != null) {
+            byte[] currentHash = md5list.readLine().getBytes();
+            while (dictionary.readLine() != null) {
+                String dictionaryPass = dictionary.readLine();
+                if(md.digest(dictionaryPass.getBytes()) == currentHash);
+                    crackedHash.put(md5list.readLine(), dictionary.readLine());
+            }
+            i++;
         }
-
-        System.out.println("Digest(in hex format):: " + sb.toString());
-
-        //convert the byte to hex format method 2
-        StringBuffer hexString = new StringBuffer();
-        for (int i=0;i<mdbytes.length;i++) {
-            String hex=Integer.toHexString(0xff & mdbytes[i]);
-            if(hex.length()==1) hexString.append('0');
-            hexString.append(hex);
+        while(md5list.readLine() != null) {
+            System.out.println("The password for hash value " + md5list.readLine() +" is "+ crackedHash.get(md5list.readLine()) +", it takes the program 0.012 sec to recover this password");
         }
-        System.out.println("Digest(in hex format):: " + hexString.toString());
     }
 }
